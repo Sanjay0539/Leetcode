58f808ac7b6b23a29b781a2f23944d6c40dce1d9
Sanjay U <su8446182@gmail.com>
2026-06-02 17:43:20 +0530

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,15 +1,16 @@
-public class SingleNumber {
-    // class Solution {
-        public static int singleNumber(int[] nums) {
-         int value = 0;
-         for(int i =0;i<nums.length;i++){
-            value = value^nums[i];
-         }   
-         return value;
+class Solution {
+    public static int singleNumber(int[] nums) {
+        int value = 0;
+        for(int i=0 ; i<nums.length;i++){
+        value = value ^ nums[i];
         }
-        public static void main(String args[]){
-            int nums[] = {4,1,2,1,2};
-            System.out.println(singleNumber(nums));
-        }
+        return value;
+    }
+    public static void main(String args[]){
+        int nums [] = {4,1,2,1,2};
+        System.out.println( singleNumber (nums));
+
     }
 
+        
+}
