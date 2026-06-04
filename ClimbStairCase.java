da143bdf0265c532f70da906aaf9b05db1c6acb0
Sanjay U <su8446182@gmail.com>
2026-06-04 22:20:06 +0530

    Update ClimbStairCase.java

--- ClimbStairCase.java
+++ ClimbStairCase.java
@@ -1,18 +1,16 @@
-public class ClimbStairCase {
-
-    public static  int climbStairs(int n) {
+class Solution {
+    public static int climbStairs(int n) {
         if (n<=3){
             return n;
         }
-        int a=3, b=2;
-        for(int i=0;i<n-3;i++){
-            a=a+b;
-            b=a-b;
-            // b++;wrong for bigger values
+        int a = 3 , b=2;
+        for(int i=0 ; i<n-3 ; i++){
+            a = a+b;
+            b =a-b;
         }
-        return a;
+        return a ;
     }
-    public static void main(String args[] ){
-        System.out.println(climbStairs(31));
+    public static void main(String args[]){
+        System.out.println( climbStairs(4));
     }
 }
